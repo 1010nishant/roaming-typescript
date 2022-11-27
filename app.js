05cb071140c1ecbb575536dd353e7cdd9db65f21
nishantjangid6377 <nishantjangid6377@gmail.com>
2022-11-27 16:08:51 +0530

    fnction commit

---
+++ app.js
@@ -0,0 +1,4 @@
+var app = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(app(10, 20));
---
+++ app.ts
@@ -0,0 +1,4 @@
+const app = (num1: number, num2: number): number => {
+    return num1 + num2
+}
+console.log(app(10, 20))
