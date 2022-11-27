c1daf6303bcf47d11b35b087f6c17a345de7b5de
nishantjangid6377 <nishantjangid6377@gmail.com>
2022-11-27 17:05:45 +0530

    #

--- app.js
+++
@@ -1,4 +0,0 @@
-var app = function (num1, num2) {
-    return num1 + num2;
-};
-console.log(app(10, 20));
--- app.ts
+++
@@ -1,5 +0,0 @@
-const app = (num1: number, num2: number): number => {
-    return num1 + num2
-}
-console.log(app(10, 20))
-
